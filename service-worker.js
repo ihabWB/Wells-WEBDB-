e6fd6141009dc7440075905b62b1b0d7510d4ea1
ihabWB <113354218+ihabWB@users.noreply.github.com>
2025-09-19 19:45:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
 // Basic offline caching (bumped version to refresh cache)
-const CACHE_NAME = 'wells-pwa-v3';
+const CACHE_NAME = 'wells-pwa-v5'; // bump this
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.webmanifest',
-  './assets/logo PWA.png' // cache your logo
+  './assets/logo PWA.png' // include the logo
 ];
+
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
